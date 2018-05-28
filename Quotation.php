4e0cf4edc4641b7d8c321c2c275bdcd76d79b670
Code dạo kiếm cơm <zinzinx8@gmail.com>
2018-05-28 09:31:49 +0700

    Update

---
+++ Quotation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * @link http://iziweb.vn
+ * @copyright Copyright (c) 2016 iziWeb
+ * @email zinzinx8@gmail.com
+ *
+ */
+namespace izi\tour;
+use Yii;
+class Quotation extends \yii\base\Component
+{
+	
+	 
+		 
+	
+}
---
+++ models/Quotation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 
+ * @link http://iziweb.vn
+ * @copyright Copyright (c) 2016 iziWeb
+ * @email zinzinx8@gmail.com
+ *
+ */
+namespace izi\tour\models;
+use Yii;
+class Quotation extends \yii\db\ActiveRecord
+{
+	
+	public static function tableName(){
+		return '{{%articles}}';
+	}
+	 
+	
+	 
+	/**
+	 * 
+	 */
+	
+	 
+		 
+	
+}
