dd944e4f20a17c26db71fab0333829b9c606f711
Code dạo kiếm cơm <zinzinx8@gmail.com>
2018-05-29 17:51:49 +0700

    update

--- Program.php
+++ Program.php
@@ -8,10 +8,75 @@
  */
 namespace izi\tour;
 use Yii;
-class Program extends \yii\base\Component
+class Program extends \yii\db\ActiveRecord
 {
-	
-	 
-		 
-	
+    
+    public function removeServiceDay($o = []){
+        $a = ['item_id','service_id','type_id','day_id','time_id','package_id'];
+        foreach ($a as $b){
+            $$b = isset($o[$b]) ? $o[$b] : 0;
+            $con[$b] = $$b;
+        }
+        Yii::$app->db->createCommand()->delete('{{%tours_programs_services_prices}}',$con)->execute();
+        Yii::$app->db->createCommand()->delete('{{%tours_programs_services_days}}',$con)->execute();
+    }
+    
+    public function insertServiceDay($o = []){
+        $a = ['item_id','service_id','type_id','day_id','time_id','package_id'];
+        foreach ($a as $b){
+            $$b = isset($o[$b]) ? $o[$b] : 0;
+            $con[$b] = $$b;
+        }
+        //
+        $a = ['quantity','price1','currency','sub_item_id'];
+        
+    }
+    
+    /**
+     * Update service day
+     * @param array $o
+     */
+    public function changeServiceDay($param, $o = []){
+        $a = ['item_id','type_id','day_id','time_id','package_id'];
+        foreach ($a as $b){
+            $$b = isset($o[$b]) ? $o[$b] : 0;
+            $con[$b] = $$b;
+        }
+        /**
+         * Check existed
+         */
+        if((new \yii\db\Query())->from('{{%tours_programs_services_prices}}')->where($con)->count(1) == 0){
+            Yii::$app->db->createCommand()->insert('{{%tours_programs_services_prices}}',$con)->execute();
+        }
+        
+        Yii::$app->db->createCommand()->update('{{%tours_programs_services_days}}',$param,$con)->execute();
+        $param['supplier_id'] = Yii::$app->tour->getSupplierIDFromService($param['service_id'],$type_id);
+        Yii::$app->db->createCommand()->update('{{%tours_programs_services_prices}}',$param,$con)->execute();
+    }
+    
+    
+    
+    
+    public function removeServiceDayPrice($o = []){
+        $a = ['item_id','service_id','type_id','day_id','time_id','package_id'];
+        foreach ($a as $b){
+            $$b = isset($o[$b]) ? $o[$b] : 0;
+            $con[$b] = $$b;
+        }
+        Yii::$app->db->createCommand()->delete('{{%tours_programs_services_prices}}',$con)->execute();
+    }
+    
+    public function insertServiceDayPrice($o = []){
+        $a = ['item_id','service_id','type_id','day_id','time_id','package_id','quantity','price1','sub_item_id'];
+        foreach ($a as $b){
+            $$b = isset($o[$b]) ? $o[$b] : 0;
+            $con[$b] = $$b;
+        }
+        //
+        $currency = isset($o['currency']) ? $o['currency'] : 1;
+        
+        
+        
+    }
+    
 }
--- Tour.php
+++ Tour.php
@@ -10,10 +10,57 @@ namespace izi\tour;
 use Yii;
 class Tour extends \yii\base\Component
 {
-	
-	 //public $program, $quotation;
 	 
+    private $_program, $_quotation;
 	 
-		 
+	public function getProgram(){
+	    if($this->_program === null){
+	        $this->_program = new \izi\tour\Program();
+	    }
+	    return $this->_program;
+	}
 	
+	public function setProgram($param){
+	    
+	    $this->_program = $param;
+	    
+	}
+	
+	public function getQuotation(){
+	    if($this->_quotation === null){
+	        $this->_quotation = new \izi\tour\Quotation();
+	    }
+	    return $this->_quotation;
+	}
+	
+	public function setQuotation($param){
+	    
+	    $this->_quotation = $param;
+	    
+	}
+	public function getSupplierIDFromService($id = 0,$type = 0){
+	    $supplier_id = 0;
+	    switch ($type){
+	        case TYPE_ID_SCEN: // Thang canh
+	        case TYPE_ID_TRAIN: // Ve tau hoa
+	            $supplier_id = (new \yii\db\Query())->from('tickets_to_suppliers')->where([
+	            'ticket_id'=>$id
+	            ])->select('supplier_id')->scalar();
+	            break;
+	        case TYPE_ID_GUIDES: //HDV
+	            
+	            $supplier_id = (new \yii\db\Query())->from('guides_to_suppliers')->where([
+	            'guide_id'=>$id
+	            ])->select('supplier_id')->scalar();
+	            break;
+	        case TYPE_ID_SHIP:
+	            $supplier_id = (new \yii\db\Query())->from('distances_to_suppliers')->where([
+	            'item_id'=>$id
+	            ])->select('supplier_id')->scalar();
+	            break;
+	        case TYPE_ID_TEXT: break;
+	        default: $supplier_id = $id; break;
+	    }
+	    return $supplier_id;
+	}
 }
